bf2dca81448f8978b825fc74f1183f057f11695c
ekv86 <ekv8686@gmail.com>
2023-09-02 17:56:35 +0300

    доданий аватар

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,7 @@ const ctrl = require("../../controllers/auth")
 const { validateData } = require("../../helpers/validation");
 const { registerSchema, updateSubscriptionSchema } = require('../../models/user');
 const authenticate = require('../../helpers/authenticate');
+const upload = require('../../helpers/upload');
 
 const router = express.Router()
 
@@ -11,5 +12,7 @@ router.post("/login", validateData(registerSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
 router.patch("/", authenticate, validateData(updateSubscriptionSchema), ctrl.updateSubscription)
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar)
+
 
 module.exports = router
